db7e5823aa1be245c891a03b2b91451822f73066
Mohd Faisal <96825384+MohdFaisalBidda@users.noreply.github.com>
2023-02-18 19:31:05 +0000

    Added bubbleSort, insertionSort, selectionSort 👨‍💻

---
+++ Search/LinearSearch/linearSearch.java
@@ -0,0 +1,15 @@
+package Search.LinearSearch;
+
+public class linearSearch {
+  public static void main(String[] args) {
+    int arr[] = { 5, 4, 3, 2, 1 };
+    System.out.println(arr);
+
+  }
+
+  public static void linear(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
