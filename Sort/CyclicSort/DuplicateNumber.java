f3e5c868712fc9c50f7cae5eaa3c14cc57f79826
Mohd Faisal <96825384+MohdFaisalBidda@users.noreply.github.com>
2023-02-23 18:23:17 +0000

    Added DuplicateNumber (cyclic Sort)

---
+++ Sort/CyclicSort/DuplicateNumber.java
@@ -0,0 +1,29 @@
+package Sort.CyclicSort;
+
+public class DuplicateNumber {
+  public static void main(String[] args) {
+    int[] arr = { 1, 3, 4, 2, 2 };
+    System.out.println(duplicateNumber(arr));
+  }
+
+  public static int duplicateNumber(int[] arr) {
+    int i = 0;
+    while (i < arr.length) {
+
+      if (arr[i] != i + 1) {
+        int correct = arr[i] - 1;
+        if (arr[i] != arr[correct]) {
+          int temp = arr[i];
+          arr[i] = arr[correct];
+          arr[correct] = temp;
+        } else {
+          return arr[i];
+        }
+      } else {
+        i++;
+      }
+    }
+    return -1;
+
+  }
+}
