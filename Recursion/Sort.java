c0f947fa38ff4c476a7a08d2a1e19ec09827c2ff
Faisal <biddafaisal@gmail.com>
2023-05-02 14:39:52 +0530

    Added linearSearch (recursive)

--- Recursion/Sort.java
+++ Recursion/Sort.java
@@ -1,14 +1,15 @@
 package Recursion;
 
-public class Sort{
-  public static void main(String[] args){
-    int[] arr={1,3,14,5,7,8};
-    System.out.println(isArraySorted(arr,0));
+public class Sort {
+  public static void main(String[] args) {
+    int[] arr = { 1, 3, 14, 5, 7, 8 };
+    System.out.println(isArraySorted(arr, 0));
   }
-  public static boolean isArraySorted(int[] arr,int i){
-    if(i == arr.length -1){
+
+  public static boolean isArraySorted(int[] arr, int i) {
+    if (i == arr.length - 1) {
       return true;
     }
-    return arr[i] < arr[i+1] && isArraySorted(arr,i+1);
+    return arr[i] < arr[i + 1] && isArraySorted(arr, i + 1);
   }
-} 
+}
