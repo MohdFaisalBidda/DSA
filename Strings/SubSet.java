ae780bb56c0e8fe64c7db6f318b7c650ab09d106
MohdFaisalBidda <biddafaisal@gmail.com>
2023-06-05 23:17:22 +0530

    Added subsets ques of strings

---
+++ Strings/SubSet.java
@@ -0,0 +1,64 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubSet {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2 };
+        List<List<Integer>> ans = subsetDuplicate(arr);
+        for (List<Integer> list : ans) {
+            System.out.println(list);
+        }
+    }
+
+    /*There are two lists where the outer list contain the answer of the previous number possible subset in the list and the internal list contains the possible subsets of the number the loop is at in arr then the internal subsets are added in outer list.
+    */
+
+    public static List<List<Integer>> subset(int[] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+
+        for (int num : arr) {
+
+            int n = outer.size();
+
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+    /*First the array is been sorted becoz the numbers in array need to be adjacent/together to track the duplicated arrays are available or not.
+     Two pointers are used start and end for tracking the duplicates internal arrays which need to be avoided
+
+     */
+
+    public static List<List<Integer>> subsetDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start =0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
